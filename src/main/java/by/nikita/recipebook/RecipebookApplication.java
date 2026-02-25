d2f9e74de706aa4b2d528dd1ffe93dfb2b85b9be
Nikita <mikhaylovnikita05@gmail.com>
2026-02-25 11:13:09 +0300

    checksstyle fix

--- src/main/java/by/nikita/recipebook/RecipebookApplication.java
+++ src/main/java/by/nikita/recipebook/RecipebookApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipebookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecipebookApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RecipebookApplication.class, args);
+    }
 }
